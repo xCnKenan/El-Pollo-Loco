3cd591f927a2ac63164e7b645c3aabb2d6206cc5
xCnKenan <cn.kenan@icloud.com>
2025-08-08 13:57:34 +0200

    latest save

--- levels/level1.js
+++ levels/level1.js
@@ -3,7 +3,9 @@ const level1 = new Level(
     // new Chicken(),
     // new Chicken(), 
     // new Chicken(),
-    new ChickenSmall()
+    new ChickenSmall(),
+    // new ChickenSmall(),
+    // new ChickenSmall(),
     // new Endboss(),
   ],
   [
