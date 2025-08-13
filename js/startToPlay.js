47990f35633b2a2a8b244a5c889a14ebd46f8bf8
xCnKenan <cn.kenan@icloud.com>
2025-08-13 19:08:46 +0200

    toggle controlls overlay and remove overlay after game started

--- js/startToPlay.js
+++ js/startToPlay.js
@@ -1,10 +1,18 @@
 function removeButtons(){
     let canvasRef = document.getElementById('canvas');
     canvasRef.style.backgroundImage = 'none';
+    let controllsOverlayRef = document.getElementById('controllsOverlay');
+    controllsOverlayRef.classList.add('d-none');
+    remove('startButton');
+    remove('controllsButton');
+}
 
-    let startButtonRef = document.getElementById('startButton');
-    startButtonRef.remove();
+function toggleOverlay(){
+    let controllsOverlayRef = document.getElementById('controllsOverlay');
+    controllsOverlayRef.classList.toggle('d-none');
+}
 
-    let controllsButtonRef = document.getElementById('controllsButton');
-    controllsButtonRef.remove();
+function remove(id){
+    let idRef = document.getElementById(id);
+    idRef.remove();
 }
