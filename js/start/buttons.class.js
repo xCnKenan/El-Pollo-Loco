bdf8b1a51efb7afe24f7c1e051fe27b4034b0dd7
xCnKenan <cn.kenan@icloud.com>
2025-08-23 21:56:27 +0200

    JSDoc for buttons.class.js

--- js/start/buttons.class.js
+++ js/start/buttons.class.js
@@ -1,3 +1,8 @@
+/**
+ * Represents a clickable button element in the game.
+ *
+ * @class Buttons
+ */
 class Buttons {
   x = 100;
   y = 100;
@@ -5,6 +10,13 @@ class Buttons {
   height;
   width;
 
+   /**
+   * Loads an image for the button.
+   *
+   * @method loadStartImage
+   * @param {string} path - The file path or URL of the image.
+   * @returns {void}
+   */
   loadStartImage(path) {
     this.img = new Image();
     this.img.src = path;
