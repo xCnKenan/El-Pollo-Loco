fd712af3bc20a1e814d875a72e7cd8c05e386a4e
xCnKenan <cn.kenan@icloud.com>
2025-08-23 22:24:44 +0200

    JSDoc for level.class.js

--- js/models/level.class.js
+++ js/models/level.class.js
@@ -1,3 +1,6 @@
+/**
+ * Represents a game level with its objects, enemies, collectibles, and end screens.
+ */
 class Level {
   enemies;
   clouds;
@@ -8,6 +11,17 @@ class Level {
   youLost;
   youWon;
 
+    /**
+   * Creates a new level instance with all its objects, enemies, and end screens.
+   * 
+   * @param {Array<MovableObject>} enemies - List of enemies in the level.
+   * @param {Array<DrawableObject>} clouds - List of clouds in the level.
+   * @param {Array<DrawableObject>} backgroundObject - List of background objects.
+   * @param {Array<DrawableObject>} bottles - List of collectible bottles.
+   * @param {Array<DrawableObject>} coins - List of collectible coins.
+   * @param {YouLost} youLost - The "You Lost" screen object for this level.
+   * @param {YouWon} youWon - The "You Won" screen object for this level.
+   */
   constructor(
     enemies,
     clouds,
