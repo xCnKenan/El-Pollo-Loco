08b02b5062eff252a5fa9b6fd221b5fbb5b3815a
xCnKenan <cn.kenan@icloud.com>
2025-08-23 21:55:23 +0200

    JSDoc for mainScreen.class.js

--- js/start/mainScreen.class.js
+++ js/start/mainScreen.class.js
@@ -1,3 +1,9 @@
+/**
+ * Represents the game's start screen.
+ * Handles rendering of the background and screen updates.
+ *
+ * @class Mainscreen
+ */
 class Mainscreen {
   screenCanvas;
   ctxStartScreen;
@@ -7,12 +13,27 @@ class Mainscreen {
     0
   );
 
+  /**
+   * Creates an instance of the start screen.
+   *
+   * @constructor
+   * @param {HTMLCanvasElement} screenCanvas - The canvas where the start screen is drawn.
+   */
   constructor(screenCanvas) {
     this.ctxStartScreen = screenCanvas.getContext("2d");
     this.screenCanvas = screenCanvas;
     this.draw();
   }
 
+  /**
+   * Continuously draws the start screen.
+   * - Clears the canvas.
+   * - Draws the background image.
+   * - Requests the next animation frame for redrawing.
+   *
+   * @method draw
+   * @returns {void}
+   */
   draw() {
     this.ctxStartScreen.clearRect(
       0,
@@ -29,6 +50,18 @@ class Mainscreen {
     });
   }
 
+    /**
+   * Draws an element on the screen.
+   *
+   * @method showOnScreen
+   * @param {Object} elements - The drawable object.
+   * @param {HTMLImageElement} elements.img - The image to draw.
+   * @param {number} elements.x - The x-coordinate for drawing.
+   * @param {number} elements.y - The y-coordinate for drawing.
+   * @param {number} elements.height - The height of the drawn image.
+   * @param {number} elements.width - The width of the drawn image.
+   * @returns {void}
+   */
   showOnScreen(elements) {
     this.ctxStartScreen.drawImage(
       elements.img,
