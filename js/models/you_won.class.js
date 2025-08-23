94557d97f8485b839ed141a874e3183d742d65e3
xCnKenan <cn.kenan@icloud.com>
2025-08-23 21:59:11 +0200

    JSDoc for you_won.class.js

--- js/models/you_won.class.js
+++ js/models/you_won.class.js
@@ -1,7 +1,20 @@
+/**
+ * Represents the "You Won" screen object that is shown when the player wins the game.
+ * Inherits from {@link MovableObject}.
+ *
+ * @class YouWon
+ * @extends MovableObject
+ */
 class YouWon extends MovableObject {
   width = 720;
   height = 480;
 
+   /**
+   * Creates an instance of the "You Won" screen.
+   * Loads the victory image and sets its position at the bottom of the canvas.
+   *
+   * @constructor
+   */
   constructor() {
     super().loadImage("img/You won, you lost/You won A.png");
     // this.x = x;
